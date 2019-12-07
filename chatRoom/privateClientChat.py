738e510fa114ae2bd75c2d2466fea0d15c4b2654
prance101 <52561810+prance101@users.noreply.github.com>
2019-12-07 17:58:32 +0330

    start

--- chatRoom/privateClientChat.py
+++ chatRoom/privateClientChat.py
@@ -1,18 +1,23 @@
 import socket ; import time ; import sys ; import threading
 
-IP = 'localhost'
-PORT = 8200
+#IP   = '172.30.19.110'
+IP   = "localhost"
+PORT = 8210
 username = input("Enter your name: ")
+friend   = input("Enter friend name: ")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
 client_socket.setblocking(0)
 
+a = 0
+client_socket.send(bytes(username,'utf-8'))
+client_socket.send(bytes(friend,'utf-8'))
 
 def send_message():
-    while True:
-        msg = input()
-        if msg:
-            client_socket.send(bytes(username + "->" + msg, 'utf-8'))
+    if a==1:
+        while True:
+            msg = input()
+            client_socket.send(bytes(username + " :> " + msg, 'utf-8'))
 
 
 t1 = threading.Thread(target=send_message)
--- chatRoom/privateServerChat.py
+++ chatRoom/privateServerChat.py
@@ -2,20 +2,18 @@ import socket
 import time
 import select
 
-IP = 'localhost'
-PORT = 8200
+#IP = '172.30.19.110'
+IP = "localhost"
+PORT = 8210
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_socket.bind((IP, PORT))
-
 server_socket.listen(10)
-print("server up!")
+print("Server Run!")
 
 socket_list = [server_socket]
-
 clients = {}
-
+i = 0 ; onUsers = []
 
 while True:
     read_socket, write_socket, exception_socket = select.select(
@@ -23,7 +21,6 @@ while True:
     for s in read_socket:
         if s == server_socket:
             client_socket, address = server_socket.accept()
-
             if client_socket:
                 client_socket.send(bytes("welcome!", 'utf-8'))
                 socket_list.append(client_socket)
@@ -32,19 +29,25 @@ while True:
                 print("Connection Established from {}".format(address))
                 for client_sockets in clients:
                     if client_sockets != client_socket:
+                        i = i + 1
                         client_sockets.send(
                             bytes("{} joined Group!".format(address), 'utf-8'))
 
         else:
-            message = s.recv(1024)
-            if not message:
-                socket_list.remove(s)
-                del clients[s]
-                continue
-          #  print(message.decode('utf-8'))
-            for client_socket in clients:
-                if client_socket != s:
-                    client_socket.send(message)
+            if i==1 :
+                message = s.recv(1024)
+                onUsers.append(message)
+                client_socket.send(bytes("\nYour friend is online","utf-8"))
+            else:
+                message = s.recv(1024)
+                if not message:
+                    socket_list.remove(s)
+                    del clients[s]
+                    continue
+              #  print(message.decode('utf-8'))
+                for client_socket in clients:
+                    if client_socket != s:
+                        client_socket.send(message)
     for s in exception_socket:
         socket_list.remove(s)
         del clients[s]
